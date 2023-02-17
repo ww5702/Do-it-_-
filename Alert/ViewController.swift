b5b4c71d3b3db02715ae3e73f10cf5ac99d1570d
ios <wow5702@naver.com>
2023-02-17 15:39:21 +0900

    Alert Controller/Action

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -8,12 +8,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let imgOn = UIImage(named: "lamp-on.png")
+    let imgOff = UIImage(named: "lamp-off.png")
+    let imgRemove = UIImage(named: "lamp-remove.png")
+    
+    var isLampOn = true
 
+    @IBOutlet var lampImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        lampImg.image = imgOn
     }
 
-
+    @IBAction func btnLampOn(_ sender: UIButton) {
+        if isLampOn == true {
+            let lampOnAlert = UIAlertController(title: "경고", message: "현재 On상태입니다.", preferredStyle: UIAlertController.Style.alert)
+            let onAction = UIAlertAction(title: "네 알겠습니다.", style: UIAlertAction.Style.default, handler: nil)
+            lampOnAlert.addAction(onAction)
+            present(lampOnAlert, animated: true, completion: nil)
+            
+        } else {
+            lampImg.image = imgOn
+            isLampOn = true
+        }
+    }
+    
+    @IBAction func btnLampOff(_ sender: UIButton) {
+        if isLampOn == false {
+            let lampOffAlert = UIAlertController(title: "경고", message: "현재 Off상태입니다.", preferredStyle: UIAlertController.Style.alert)
+            let offAction = UIAlertAction(title: "네 알겠습니다", style: UIAlertAction.Style.default, handler: nil)
+            lampOffAlert.addAction(offAction)
+            present(lampOffAlert, animated: true, completion: nil)
+        } else {
+            let lampOffAlert2 = UIAlertController(title: "램프끄기", message: "램프를 끄시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+            let OffAction2 = UIAlertAction(title: "네", style: UIAlertAction.Style.default, handler: {
+                ACTION in self.lampImg.image = self.imgOff
+                self.isLampOn = false
+            })
+            let cancelAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.default, handler: nil)
+            
+            lampOffAlert2.addAction(OffAction2)
+            lampOffAlert2.addAction(cancelAction)
+            present(lampOffAlert2, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func btnLampRemove(_ sender: UIButton) {
+        let lampRemoveAlert = UIAlertController(title: "램프 제거", message: "램프를 제거하시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+        let offAction = UIAlertAction(title: "아니요 끄겠습니다(off)", style: UIAlertAction.Style.default, handler: {
+            ACTION in self.lampImg.image = self.imgOff
+            self.isLampOn = false
+        })
+        let onAction = UIAlertAction(title: "아니오 키겠습니다(on)", style: UIAlertAction.Style.default, handler:  {
+            ACTION in self.lampImg.image = self.imgOn
+            self.isLampOn = true
+        })
+        let removeAction = UIAlertAction(title: "네 제거하겠습니다.", style: UIAlertAction.Style.default, handler:  {
+            ACTION in self.lampImg.image = self.imgRemove
+            self.isLampOn = false
+        })
+        
+        lampRemoveAlert.addAction(offAction)
+        lampRemoveAlert.addAction(onAction)
+        lampRemoveAlert.addAction(removeAction)
+        present(lampRemoveAlert, animated: true, completion : nil)
+    }
+    
 }
 
