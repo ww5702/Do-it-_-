d70e1f13ab85ea520dd69fb9f95648acc805d1be
ios <wow5702@naver.com>
2023-02-20 15:03:38 +0900

    Web_View(초기화면 , 로딩 indicator 구현)

--- Web/ViewController.swift
+++ Web/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
+    // 시작홈페이지
+    func loadWebPage(_ url : String) {
+        let myUrl = URL(string: url)    // string값을 url로 변경
+        let myRequest = URLRequest(url: myUrl!)     //url을 myRequest형으로 선언
+        myWebView.load(myRequest)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myWebView.navigationDelegate = self
+        //Info.plist 조정해야 화면이 뜬다.
+        loadWebPage("http://2sam.net")
+    }
+    
+    // 로딩보이기
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        myActivityIndicator.startAnimating()
+        myActivityIndicator.isHidden = false
+    }
+    // 로딩이 완료되었을때 인디케이터를 중지하고 숨김
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+    }
+    // 로딩 실패시 동작하므로 인디케이터를 중지하고 숨김
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
     }
 
-
+    
+    
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+    }
 }
 
