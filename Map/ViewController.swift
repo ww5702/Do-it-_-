d22d1983d030deb31458edbae357d4cc6b5599d9
ios <wow5702@naver.com>
2023-02-21 10:58:50 +0900

    Map_View(위도/경도, 위치업데이트)

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -30,7 +30,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()                     // 위치 업데이트를 시작
         myMap.showsUserLocation = true                              // 위치 보기 값을 true로 설정
     }
+    
+    // 위도 경도 표시
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span : Double) {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)       // 위도, 경도 값을 매개변수로 함수 호출
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)     // 범위 값을 매개변수로 함수 호출
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)            // plocation, spanvalue 값을 매개변수로 함수 호출
+        myMap.setRegion(pRegion, animated: true)
+    }
 
+    // 위치가 업데이트 되었을 때 지도에 위치 나타내기
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pLocation = locations.last  // 위치가 바뀌면 마지막 위치값을 찾아낸다.
+        // 마지막 위도 경도로 지도에 표시, delta 0.01은 1의 값보다 100배 확대된 값으로 표현
+        goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+    }
 
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
     }
