ccf606fa493e1955670cfd8949a7ee295cf27397
ios <wow5702@naver.com>
2023-02-21 11:05:37 +0900

    Map_View(현재위치)

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -44,6 +44,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let pLocation = locations.last  // 위치가 바뀌면 마지막 위치값을 찾아낸다.
         // 마지막 위도 경도로 지도에 표시, delta 0.01은 1의 값보다 100배 확대된 값으로 표현
         goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        
+        // 위치 정보 추출하여 텍스트로
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
+            (placemarks, error) -> Void in
+            let pm = placemarks!.first              // placemark 값의 첫부분만 pm으로
+            let country = pm!.country               // pm상수에서 나라값을 country로
+            var address:String = country!           // address에 country값 대입
+            if pm!.locality != nil {                // pm에 지역값이 존재하면 address에 추가
+                address += " "
+                address += pm!.locality!
+            }
+            if pm!.thoroughfare != nil {            // pm에 도로값이 존재하면 address에 추가
+                address += " "
+                address += pm!.thoroughfare!
+            }
+            
+            self.lblLocationInfo1.text = "현재 위치"
+            self.lblLocationInfo2.text = address
+        })
+        locationManager.stopUpdatingHeading()
     }
 
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
