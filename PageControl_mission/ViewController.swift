60079b64ddcd8474eda1d00575816779c5c5530f
ios <wow5702@naver.com>
2023-02-21 15:26:42 +0900

    Page_Control_Mission

--- PageControl_mission/ViewController.swift
+++ PageControl_mission/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelValue: UILabel!
+    @IBOutlet var pageControlValue: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pageControlValue.numberOfPages = 10
+        pageControlValue.currentPage = 0
+        pageControlValue.pageIndicatorTintColor = UIColor.blue
+        pageControlValue.currentPageIndicatorTintColor = UIColor.yellow
+        
+        labelValue.text = String(pageControlValue.currentPage+1)
     }
 
-
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        labelValue.text = ("\(pageControlValue.currentPage+1)")
+    }
+    
 }
 
