0864f1b516d855f00dff8ce23d13cabfff20cd73
ios <wow5702@naver.com>
2023-02-24 15:16:47 +0900

    Initial Commit

---
+++ MoviePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by 이재웅 on 2023/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
