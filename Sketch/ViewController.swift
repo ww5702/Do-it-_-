f9300564b21c2ad723446b4cdcc52dfee95dae8e
ios <wow5702@naver.com>
2023-02-27 16:46:46 +0900

    Sketch

--- Sketch/ViewController.swift
+++ Sketch/ViewController.swift
@@ -9,11 +9,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgView: UIImageView!
+    
+    var lastPoint: CGPoint!
+    var lineSize: CGFloat = 2.0
+    var lineColor = UIColor.red.cgColor
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func btnClearImageView(_ sender: UIButton) {
+        imgView.image = nil
+    }
+    
+    // 터치 이벤트 메서드 재정의
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        lastPoint = touch.location(in: imgView)     // 터치된 위치를 lastPoint에 저장
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)      // 라인의 끝 모양을 라운드로
+        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
+        let touch = touches.first! as UITouch
+        let currPoint = touch.location(in: imgView)
+        
+        imgView.image?.draw(in: CGRect(x: 0, y: 0, width: imgView.frame.size.width, height: imgView.frame.size.height))
+        
+        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: currPoint.x, y: currPoint.y))
+        UIGraphicsGetCurrentContext()?.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        lastPoint = currPoint
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
+        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
+        
+        imgView.image?.draw(in: CGRect(x: 0, y: 0, width: imgView.frame.size.width, height: imgView.frame.size.height))
+        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
 
-
+    }
+    // 사용자가 핸드폰을 흔들었을때 이미지 뷰 위에 그렸던 그림 지워지기
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            imgView.image = nil
+        }
+    }
+    
+    
+    @IBAction func btnColorBlack(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnColorRed(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnColorGreen(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnColorBlue(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnSetLineWidth(_ sender: UIButton) {
+    }
+    
+    
 }
 
