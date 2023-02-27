815e52594b8370f12afbbf7c2e1fd4c56539438c
ios <wow5702@naver.com>
2023-02-27 16:04:15 +0900

    TapTouch

--- TapTouch/ViewController.swift
+++ TapTouch/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtMessage: UILabel!
+    
+    @IBOutlet var txtTapCount: UILabel!
+    
+    @IBOutlet var txtTouchCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // 터치가 시작될때 호출
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Began"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    // 터치된 손가락이 움직였을 때 호출
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Moved"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    // 화면에서 손가락을 떼었을 떄 호출
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Ended"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
 
 }
 
