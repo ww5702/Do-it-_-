e2666e6675631ce25ca661d277d61c01376d64d5
ios <wow5702@naver.com>
2023-03-01 15:25:35 +0900

    PinchGesture

--- PinchGesture/ViewController.swift
+++ PinchGesture/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtPincch: UILabel!
+    
+    var initialFontSize: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        self.view.addGestureRecognizer(pinch)
+    }
+
+    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        if(pinch.state == UIGestureRecognizer.State.began) {
+            initialFontSize = txtPincch.font.pointSize                                  // 현재 폰트사이즈 저장
+        } else {
+            txtPincch.font = txtPincch.font.withSize(initialFontSize * pinch.scale)
+        }
     }
 
 
